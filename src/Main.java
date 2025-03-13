f603434d348513147ed433f61c2de41393cb39e5
TheCobraBoy2 <landoncs10@gmail.com>
2025-03-12 22:13:30 -0500

    Ternary Operator

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /* EXERCISE 1: Simple Calculator
-        *
-        * Create a Calculator that runs multiple times until you manually break out of it.
-        * You should read the operation first, then read the first and second number.
-        *
-        * Depending on the operation chosen, you then have to return a different result.
-        * You can do this with if statements or switch statements.
-        *
-        * Please note that when reading a String, you want to use scanner.next()
-        * And when comparing strings you want to use:
-        * String s = "something";
-        * s.equals("something");
-        * and not ==
-        *
-        * After the result has be output, ask the user if they want to continue, if not end the program!
-        * Estimated Time: 30-45 minutes
-        * */
+        /* TERNARY OPERATOR */
 
+        int health = 3;
+        String status = "";
+        System.out.println("What is your status? " + status);
+
+        // No Ternary Operator
+        if (health > 0) {
+            status = "Alive";
+        } else {
+            status = "Non-Alive";
+        }
+
+        System.out.println("What is your status? " + status);
+        status = "";
+
+        health = 0;
+        // Ternary Operator / One line if else statement
+        status = health > 0 ? "Alive" : "Non-Alive";
+        System.out.println("What is your status? " + status);
     }
 }
