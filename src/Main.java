6bd6905089012bc18daecf32a83e044766bfa694
TheCobraBoy2 <landoncs10@gmail.com>
2025-03-12 20:44:29 -0500

    Integers and Math

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,44 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        /* INPUT & OUTPUT */
+        /* INTEGERS & MATH */
 
-        /* OUTPUT */
-        // Outputting a string
-        System.out.println("Hello, this is outputting a string to the terminal in Java.");
+        int a = 100;
+        int b = 200;
 
-        // Outputting an integer
-        int outputThis = 42;
-        System.out.println(outputThis); // Outputs the value of outputThis which in this case is an integer of 42
+        // ADDITION
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum); // Printing the sum of the two numbers (a and b)
 
-        // Output with string and integer
-        int grade = 80;
-        System.out.println("Your grade is " + grade); // Outputs Your grade is 80
+        // SUBTRACTION
+        int dif = a - b;
+        System.out.println("Difference of " + a + " and " + b + " is: " + dif); // Printing the difference of the two numbers (a and b)
 
-        /* INPUT */
-        // Create a new Scanner
-        Scanner scanner = new Scanner(System.in);
-        // Use the scanner to get the next thing the user types
-        int number = scanner.nextInt(); // Try typing a string what do you notice?
-        // Output what the user types
-        System.out.println("You just typed " + number);
+        // MULTIPLICATION
+        int product = a * b;
+        System.out.println("Product of " + a + " and " + b + " is: " + product); // Printing the product of the two numbers (a and b)
 
-        // If you want to be able to take in any data from the scanner and NOT error then
-        scanner.nextLine(); // So that both the number scanner and this one can run
-        String input = scanner.nextLine();
-        // Output the input
-        System.out.println("You now just typed " + input);
+        // DIVISION
+        int Quotient = a / b; // Is 0 because an integer is only a whole number
+        System.out.println("Quotient of " + a + " and " + b + " is: " + Quotient); // Printing the quotient of the two numbers (a and b)
+        Quotient = b / a;
+        System.out.println("Quotient of " + b + " and " + a + " is: " + Quotient); // Printing the quotient of the two numbers (a and b)
+
+        // REMAINDER (MODULUS)
+        int remainder = 100 % 2; // Can be used to figure out if a variable (number) is even or odd
+        System.out.println("Remainder from 100/2 " + remainder);
+
+        remainder = 100 % 3;
+        System.out.println("Remainder from 100/3 " + remainder);
+
+        remainder = 100 % 4;
+        System.out.println("Remainder from 100/4 " + remainder);
+
+        // MATH METHODS
+        System.out.println("Ceiling of: 1.23: " + Math.ceil(1.23)); // Round up
+        System.out.println("Round of: 1.23: " + Math.round(1.23)); // Rounding
+        System.out.println("Floor of: 1.23: " + Math.floor(1.23)); // Rounding down
+        System.out.println("Absolute of: -100: " + Math.abs(-100)); // Absolute value
+        System.out.println("Minimum of: 10 and 200 : " + Math.min(10, 200)); // Minimum
+        System.out.println("Maximum of: 10 and 200 : " + Math.max(37, 750)); // Maximum
     }
 }
