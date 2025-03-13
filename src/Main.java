544ea3e84e7de5abe85e867880cad5f97065e0a2
TheCobraBoy2 <landoncs10@gmail.com>
2025-03-12 23:15:48 -0500

    Arrays

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        /* DATA TYPES & VARIABLES */
+        /* ARRAYS */
 
-        // Hello I am a comment
-        /*
-         * And I am a multiline comment!
-         * I can come in handy for explaining stuff
-         */
+        String question1 = "What does WWW stand for?";
+        String question2 = "What is the World's Largest Ocean?";
+        String question3 = "Which Year did East and West Germany Unify?";
 
-        /* Variables */
+        String answer1 = "World Wide Web";
+        String answer2 = "Pacific Ocean";
+        String answer3 = "1990";
 
-        // datatype name_of_variable; // Declaration
-        // OR
-        // datatype name_of_variable = value_of_variable; // Initialization (Declaration + Assignment of Value)
+        // Now add 15 more ...   Don't actually just get into the mindset of possibly having to add the small amount of 15 more questions
+        // How do we print these easily?
 
-        /* DATA TYPES */
+        for (int i = 0; i < 3; i++) {
+            // System.out.println(questioni); Well this doesn't work so hmm how do we do this
+        }
 
-        // Integers (Store whole numbers (including negative ones))
-        // Goes from -2147483648 to 2147483648
-        int points;
-        points = 100;
-        int x = -42;
+        // Array - Think of this as a list
+        String[] questions = new String[3]; // Makes a new array called questions of size 3
+        // The array starts with index 0 not 1
+        // Basically every programming language starts at index 0 but a select few need to be special and start at something like 1 an example would be lua
+        questions[0] = "What does WWW stand for?";
+        questions[1] = "What is the World's Largest Ocean?";
+        questions[2] = "Which Year did East and West Germany Unify?";
 
-        // Floating Point Numbers (float & double)
-        float pi = 3.14159265f;
-        double e = 2.718281828;
+        String[] answers = new String[3]; // Makes a new array called answers of size 3
+        answers[0] = "World Wide Web";
+        answers[1] = "Pacific Ocean";
+        answers[2] = "1990";
 
-        // For real numbers, however! Precision can be an issue!
-        // Scientific notation: 314159265 * 10^-8
-
-        // Boleans (True or False)
-        boolean learningIsFun = true;
-        boolean CSSIsFun = false;
-
-        // Chars & Strings
-        char grade = 'A';
-        String name = "VorrikZ";
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Q: " + questions[i] + " | A: " + answers[i]); // Much better this works unlike the other for loop
+        }
     }
 }
