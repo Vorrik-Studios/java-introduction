0d8b992020ec09dd7414ca513233dec150fbf28c
TheCobraBoy2 <landoncs10@gmail.com>
2025-03-12 19:40:16 -0500

    Data Types and Variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        /* DATA TYPES & VARIABLES */
+
+        // Hello I am a comment
+        /*
+         * And I am a multiline comment!
+         * I can come in handy for explaining stuff
+         */
+
+        /* Variables */
+
+        // datatype name_of_variable; // Declaration
+        // OR
+        // datatype name_of_variable = value_of_variable; // Initialization (Declaration + Assignment of Value)
+
+        /* DATA TYPES */
+
+        // Integers (Store whole numbers (including negative ones))
+        // Goes from -2147483648 to 2147483648
+        int points;
+        points = 100;
+        int x = -42;
+
+        // Floating Point Numbers (float & double)
+        float pi = 3.14159265f;
+        double e = 2.718281828;
+
+        // For real numbers, however! Precision can be an issue!
+        // Scientific notation: 314159265 * 10^-8
+
+        // Boleans (True or False)
+        boolean learningIsFun = true;
+        boolean CSSIsFun = false;
+
+        // Chars & Strings
+        char grade = 'A';
+        String name = "VorrikZ";
     }
 }
