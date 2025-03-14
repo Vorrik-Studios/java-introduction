e59a60fd468764ca42340f6bdca3b102945ddb4f
TheCobraBoy2 <landoncs10@gmail.com>
2025-03-13 23:03:01 -0500

    More things to consider

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,29 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        /* JSON */
+        /* Final part of the introduction */
 
-        // Look in resources/steve.json
+        // I missed an access modifier of final
+        // Basically final can never have a new value its constant
+        final int x = 30;
+        // Null means something has no value
+        List<String> names = null;
+
+        // var is a variable of any type
+        var y = 5.5;
+
+        /* MORE THINGS TO LOOK AT */
+
+        /*
+         These can be some more advanced topics or topics that might be used for fields that use Java
+         I still even if you think you won't be using Java would recommend looking to these to widen your understanding
+         You never know what you might end up doing for work
+
+         Design Patterns for Java (Used more for software engineering and comp sci) -> https://www.geeksforgeeks.org/java-design-patterns/
+         Singleton Pattern (Used alot in Minecraft modding) -> https://www.baeldung.com/java-singleton
+         Java Conventions (Just used for everything) -> https://google.github.io/styleguide/javaguide.html
+        */
 
     }
 }
