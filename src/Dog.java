6cfb4b4267019982ba1537a0e091dbecbc254642
TheCobraBoy2 <landoncs10@gmail.com>
2025-03-13 20:17:14 -0500

    Access Modifiers

--- src/Dog.java
+++ src/Dog.java
@@ -1,15 +1,30 @@
 public class Dog {
-    public String picture;
-    public String name;
-    public int age;
+    public String picture; // accessible from anywhere and changeable
+    protected String name; // accessible in subclasses and same package
+    private int age;       // accessible ONLY withing the class itself
+
+    public static int numberOfDogs = 0; // Means there is one copy shared by all dogs
+    // Basically a global value among the class in this case Dog
 
     public Dog(String picture, String name, int age) {
         this.picture = picture;
         this.name = name;
         this.age = age;
+
+        numberOfDogs++;
     }
 
     public void birthday() {
         age++;
+    }
+
+    // Setter
+    public void setAge(int num) {
+        this.age = num;
+    }
+
+    // Getter
+    public int getAge() {
+        return age;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,26 @@ public class Main {
         * Take a look at this PowerPoint: https://1drv.ms/p/c/1ac3dee98978c804/ERr8WXvEONtOrsE_vFtG5okB8x3nJccACL-shwzMViXEBg?e=p6YzdD
         */
 
-        /* CLASSES & OBJECTS */
-        // CLASSES SHOULD ALWAYS START WITH A CAPITAL LETTER
-        // This time you have to look at multiple files
+        /* ACCESS MODIFIERS */
+
+        System.out.println("How many dogs are there? " + Dog.numberOfDogs);
 
         Dog bengie = new Dog("bengie.png", "Bengie", 9);
         Dog jeremy = new Dog("jeremy.png", "Jeremy", 7);
 
-        System.out.println("Bengie age " + bengie.age);
-        System.out.println("Jeremy age " + jeremy.age);
+        System.out.println("Bengie age " + bengie.getAge());
+        System.out.println("Jeremy age " + jeremy.getAge());
 
         bengie.birthday();
 
-        System.out.println("Bengie age " + bengie.age);
-        System.out.println("Jeremy age " + jeremy.age);
+        System.out.println("Bengie age " + bengie.getAge());
+        System.out.println("Jeremy age " + jeremy.getAge());
 
-        jeremy.age = 100;
+        System.out.println("How many dogs are there? " + Dog.numberOfDogs);
 
-        System.out.println("Bengie age " + bengie.age);
-        System.out.println("Jeremy age " + jeremy.age);
+        Dog charlie = new Dog("charlie.png", "Charlie", 11);
+
+        System.out.println("How many dogs are there? " + Dog.numberOfDogs);
+
     }
 }
